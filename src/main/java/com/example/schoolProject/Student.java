ccecb9cd93090218b0ecef12595a6bbc8659b374
talhaozbay <oyun_kolu_44@hotmail.com>
2021-08-02 10:13:38 +0300

    firstCommit

---
+++ src/main/java/com/example/schoolProject/Student.java
@@ -0,0 +1,92 @@
+package com.example.schoolProject;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "name", length = 32, nullable = false)
+    private String name;
+
+    @Column(name = "surname", length = 64, nullable = false)
+    private String surname;
+
+    @Column(name = "age", nullable = true)
+    private Integer age;
+
+    @Column(name = "gpa", nullable = true)
+    private double gpa;
+
+    @Column(name = "number", nullable = true)
+    private String number;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, String surname, Integer age, double gpa, String number) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gpa = gpa;
+        this.number = number;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
