94a4aebd663954b98139f55902f2433eb763f44b
talhaozbay <oyun_kolu_44@hotmail.com>
2021-08-03 14:20:42 +0300

    hebele hübele

--- src/test/java/com/example/schoolProject/SchoolProjectApplicationTests.java
+++ src/test/java/com/example/schoolProject/SchoolProjectApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.schoolProject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SchoolProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@LocalServerPort
+	private int port;
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class))
+				.contains("Hello World!");
+	}
 }
